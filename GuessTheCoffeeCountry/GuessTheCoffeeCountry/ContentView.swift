c93bd459dfb0d322cdec1382504f76461ce4faf0
laurent <wrmultitudes@gmail.com>
2020-01-14 20:46:51 +0100

    first draft

--- GuessTheCoffeeCountry/GuessTheCoffeeCountry/ContentView.swift
+++ GuessTheCoffeeCountry/GuessTheCoffeeCountry/ContentView.swift
@@ -8,23 +8,51 @@
 
 import SwiftUI
 
-let date = Date()
-let components = Calendar.current.dateComponents([.hour,.minute], from: date)
 
 struct ContentView: View {
     
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var coffees = ["AbelSanchez","Aramosa","Chania","Coocamu","Edido","DambiUddo","ElDurazno","ElMarcelo","ElMirador","ElTito","Himalaya","IvanDosSantos"].shuffled()
+    
+    @State private var correctAnswer = Int.random(in: 0...2)
     
     var body: some View {
-        Form{
-            Section {
-                
-                Text("Hello, World!")
-                Text("Date \(date)")
-                //Text("Hour \(components.hour ?? 0)")
-                
+        ZStack{
+            Color.blue.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 20) {
+                VStack {
+                    Text("Tap the country for the coffee:").foregroundColor(.white)
+                    Text("\(coffees[correctAnswer])").foregroundColor(.white)
+                }
+                ForEach(0..<3){ number in
+                    Button(action: {
+                        self.flagTapped(number)
+                    }) {
+                Image(self.coffees[number]).renderingMode(.original)
+                    }
+                }
+                Spacer()
             }
         }
-        
+        .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Your Score is ??? "), dismissButton: .default(Text("Continue")))
+            }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        coffees.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
@@ -33,3 +61,19 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+//        ZStack{
+//            AngularGradient(gradient: Gradient(colors: [.red, .green, .blue, .pink]), center: .center).edgesIgnoringSafeArea(.all)
+//            VStack{
+//                Button(action: {
+//                    self.showingAlert = true
+//                }) {
+//                       Text("show Alert")
+//                        .alert(isPresented: $showingAlert){  Alert(title: Text("alert"), message: Text("This is some alert"), dismissButton: .default(Text("ok")))
+//                    }
+//                }
+//                            Spacer()
+//            }
+//        }
+//        RadialGradient(gradient: Gradient(colors: [.red, .blue, .pink, .yellow]), center: .trailing, startRadius: 20, endRadius: 200)
