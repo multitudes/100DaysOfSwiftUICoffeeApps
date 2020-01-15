4fc1d42d7b0617084b71c83d03883b872b32382e
laurent <wrmultitudes@gmail.com>
2020-01-15 15:33:00 +0100

    coffee countries

--- GuessTheCoffeeCountry/GuessTheCoffeeCountry/ContentView.swift
+++ GuessTheCoffeeCountry/GuessTheCoffeeCountry/ContentView.swift
@@ -10,47 +10,102 @@ import SwiftUI
 
 
 struct ContentView: View {
-    
+    @State var allowhittesting = true
     @State private var showingScore = false
-    @State private var scoreTitle = ""
-    
+    @State private var scoreTitle = "Ready?"
+    @State private var alertMessage = "Not bad but you can do better!"
     @State private var coffees = ["AbelSanchez","Aramosa","Chania","Coocamu","Edido","DambiUddo","ElDurazno","ElMarcelo","ElMirador","ElTito","Himalaya","IvanDosSantos"].shuffled()
     
     @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var tries = 0
+    var showMessage: String {
+        return scoreTitle
+    }
+    
+    struct CoffeeMapImage: View {
+        var coffee: String
+        
+        var body: some View {
+          Image(self.coffee).renderingMode(.original).clipShape(Rectangle())
+            .overlay(Rectangle().stroke(Color.blue, lineWidth: 1)).shadow(color: .secondary, radius: 0.5)
+        }
+        
+    }
     
     var body: some View {
         ZStack{
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
             VStack(spacing: 20) {
                 VStack {
-                    Text("Tap the country for the coffee:").foregroundColor(.white)
+                    Text("\nTap the origin for this coffee:\n").foregroundColor(.white).fontWeight(.bold)
                     Text("\(coffees[correctAnswer])").foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                    Text("current score \(score)")
+                        .foregroundColor(.white)
+                    Text(scoreTitle)
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .lineLimit(2)
+                        .multilineTextAlignment(.center)
                 }
                 ForEach(0..<3){ number in
                     Button(action: {
                         self.flagTapped(number)
                     }) {
-                Image(self.coffees[number]).renderingMode(.original)
+                        CoffeeMapImage(coffee: self.coffees[number])
                     }
+                    .allowsHitTesting(self.allowhittesting ? true : false)
                 }
+
                 Spacer()
             }
         }
         .alert(isPresented: $showingScore) {
-            Alert(title: Text(scoreTitle), message: Text("Your Score is ??? "), dismissButton: .default(Text("Continue")))
-            }
+            Alert(title: Text("Game Over"), message: Text(alertMessage), dismissButton: .default(Text("Continue")){
+                self.askQuestion()
+                self.score = 0
+                self.tries = 0
+                })
+            
+        }
     }
     
     func flagTapped(_ number: Int) {
+        allowhittesting = false
+        tries += 1
         if number == correctAnswer {
             scoreTitle = "Correct"
+            score += 1
         } else {
             scoreTitle = "Wrong"
+            score -= 1
         }
-        showingScore = true
+        if tries == 10 {
+            if score == 10 {
+                alertMessage = "Score: \(score)\nVery good! 👍🏻"
+            }
+            if score > 7 && score != 10 {
+                alertMessage = "Score: \(score)\nNot bad but you can do better!"
+            }
+            if score <= 7 {
+                alertMessage = "Score: \(score)\nNice try but better you keep study hard! 😬"
+            }
+            showingScore = true
+            scoreTitle = "wait..."
+        }
+        if showingScore == false {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.75) {
+            self.askQuestion()
+            }
+        }
     }
     
     func askQuestion() {
+        allowhittesting = true
+        scoreTitle = "Go!"
         coffees.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
@@ -62,18 +117,3 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
-//        ZStack{
-//            AngularGradient(gradient: Gradient(colors: [.red, .green, .blue, .pink]), center: .center).edgesIgnoringSafeArea(.all)
-//            VStack{
-//                Button(action: {
-//                    self.showingAlert = true
-//                }) {
-//                       Text("show Alert")
-//                        .alert(isPresented: $showingAlert){  Alert(title: Text("alert"), message: Text("This is some alert"), dismissButton: .default(Text("ok")))
-//                    }
-//                }
-//                            Spacer()
-//            }
-//        }
-//        RadialGradient(gradient: Gradient(colors: [.red, .blue, .pink, .yellow]), center: .trailing, startRadius: 20, endRadius: 200)
---
+++ GuessTheCoffeeCountry/readme.md
@@ -0,0 +1,12 @@
+
+# Guess The Coffee Country
+
+I wanted to practice SwiftUI with the 100 Days of SwiftUI course by Paul Hudson and at the same time have some fun and do something useful.
+This little coffee app has been inspired by the coffees offered by [The Barn](https://thebarn.de/pages/coffee-card-archive) in Berlin
+
+<p align="center">
+  <img src="/assets/GuessTheCoffeeCountry1.png" width="230"  title="Coffee Brewing Ratios">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="/assets/GuessTheCoffeeCountry2.png" width="230"  title="Coffee Brewing Ratios">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <img src="/assets/GuessTheCoffeeCountry.gif" width="230"  title="Coffee Brewing Ratios">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</p>
+
